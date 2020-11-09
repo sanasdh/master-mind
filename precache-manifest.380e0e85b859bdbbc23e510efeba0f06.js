b2f48f7d7f2021e96a6ddd3208c16c88444d2040
sanasdh <sanasdh@gmail.com>
2020-11-08 23:10:04 -0500

    Updates

---
+++ precache-manifest.380e0e85b859bdbbc23e510efeba0f06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2330b66e91b4ad978052e88c658b5fc",
+    "url": "/master-mind/index.html"
+  },
+  {
+    "revision": "c07d23eea3d18dd784a5",
+    "url": "/master-mind/static/css/main.31593bdd.chunk.css"
+  },
+  {
+    "revision": "7831bc72dc4365eda3ca",
+    "url": "/master-mind/static/js/2.e5f6a550.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/master-mind/static/js/2.e5f6a550.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c07d23eea3d18dd784a5",
+    "url": "/master-mind/static/js/main.a271c44a.chunk.js"
+  },
+  {
+    "revision": "8325dd1c8ff027b48d1f",
+    "url": "/master-mind/static/js/runtime-main.11b4da21.js"
+  }
+]);
